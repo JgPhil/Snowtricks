7774cdf00219f97b8ba1917cedbc311625b6851f
JgPhil <jamingph@gmail.com>
2020-06-28 22:23:56 +0200

    javascript

--- public/js/loadMore.js
+++ public/js/loadMore.js
@@ -3,50 +3,53 @@ const xhr = new XMLHttpRequest();
 
 const loadButton = document.querySelector("#js-load");
 let result = null;
+let more = document.querySelector("#js-more");
+
 
 loadButton.addEventListener('click', function (event) {
-    const url = this.href;
-    let more = document.querySelector("#js-more");
-    let div1,div2,img,div3,h5,link,description,button = null;
     event.stopPropagation();
     event.preventDefault();
+    const url = this.href;  
+    
     xhr.open("POST", url);
     xhr.onreadystatechange = function () {
         if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
             let response = this.response
             result = JSON.parse(response);
+
             result.forEach(function (k) {
-                div1 = document.createElement('div');
+                let div1 = document.createElement('div');
                 div1.classList.add("col-md-6", "col-lg-4");
 
-                div2 = document.createElement('div');
+                let div2 = document.createElement('div');
                 div2.classList.add("card", "mb-4");
 
-                img = document.createElement('img');
+                let img = document.createElement('img');
                 img.classList.add('card-img-top');
-                img.src = 'http://placehold.it/300x200';
+                img.src = k.pictures[0] ? k.pictures[0].name : 'http://placehold.it/300x200';
 
-                div3 = document.createElement('div');
+                let div3 = document.createElement('div');
                 div3.classList.add("card-body");
 
-                h5 = document.createElement('h5');
+                let h5 = document.createElement('h5');
                 h5.classList.add("card-title");
 
-                link = document.createElement('a');
+                let link = document.createElement('a');
                 link.classList.add("card-title");
+                link.innerHTML = k.title;
+                link.href = "/figure/" + k.id;
 
-
-                description = document.createElement('p');
-                button = document.createElement('a');
+                let description = document.createElement('p');
+                let button = document.createElement('a');
                 button.classList.add("btn", "btn-primary", "btn-lg");
-                button.href = "http://127.0.0.1:8000/figure/"+k.id;
+                button.href = "http://127.0.0.1:8000/figure/" + k.id;
                 button.text = "Voir";
-                
+
                 description.classList.add("card-text");
                 description.innerHTML = k.description;
-                link.innerHTML = k.title;
 
-                div1.appendChild(div2);                
+
+                div1.appendChild(div2);
                 div2.appendChild(img);
                 div2.appendChild(div3);
                 h5.appendChild(link);
