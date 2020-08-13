ddfec4bd71b10d492d04cbe5adc955ec85a5c225
JgPhil <jamingph@gmail.com>
2020-08-13 22:59:48 +0200

    userFixture admin code added

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -9,22 +9,32 @@ use Faker;
 
 class UserFixtures extends Fixture
 {
-
     public function load(ObjectManager $manager)
     {
+        $faker = Faker\Factory::create('fr_FR');
+        //admin
+        $admin = new User();
+        $admin->setUsername('admin');
+        $admin->setPassword(password_hash('admin', PASSWORD_BCRYPT));
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin
+            ->setEmail($faker->email())
+            ->setCreatedAt($faker->dateTimeThisMonth('now', 'Europe/Paris'));
+        $manager->persist($admin);
 
         for ($i = 0; $i < 20; $i++) {
             $user = new User();
-            $faker = Faker\Factory::create('fr_FR');
             $user->setUsername($faker->firstName());
             $user->setPassword(
                 password_hash($faker->password(), PASSWORD_BCRYPT)
             );
-            $user->setRoles(["ROLE_USER"]);
-            $user->setEmail($faker->email())
+            $user->setRoles(['ROLE_USER']);
+            $user
+                ->setEmail($faker->email())
                 ->setCreatedAt(
                     $faker->dateTimeThisMonth('now', 'Europe/Paris')
-                );
+                )
+                ;
             $manager->persist($user);
         }
         $manager->flush();
