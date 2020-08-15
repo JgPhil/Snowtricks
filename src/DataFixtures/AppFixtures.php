93df6c95e1a00353dc9ef216767b0ae61273f0a2
JgPhil <jamingph@gmail.com>
2020-08-15 23:51:36 +0200

    AppFixtures rewrited success

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -82,27 +82,27 @@ class AppFixtures extends Fixture implements DependentFixtureInterface
                         'description' =>
                             'Le backflip figure parmi les sauts les plus spectaculaires de cette discipline. Il nécessite la maîtrise des fondamentaux et d’une bonne perception du corps. En effet, avoir la tête en bas, même pendant quelques secondes seulement, est très difficile pour les non-initiés.',
                         'picture' =>
-                            'https://www.shutterstock.com/image-photo/snowboarder-jumps-backflip-laax-switzerland-392784835',
+                            'Backflip.jpg',
                         'video' =>
-                            'https://www.youtube.com/watch?v=W853WVF5AqI',
+                            'https://www.youtube.com/embed/W853WVF5AqI',
                     ],
                     [
                         'title' => 'Mc Twist',
                         'description' =>
                             "Le Mc Twist est un flip (rotation verticale) agrémenté d'une vrille. Un saut plutôt périlleux réservé aux riders les plus confirmés. Le champion Shaun White s'est illustré par un Double Mc Twist 1260 lors de sa session de Half-Pipe aux Jeux Olympiques de Vancouver en 2010.",
                         'picture' =>
-                            'https://images.app.goo.gl/8kcQs8hXEccSYcD16',
+                            'McTwist.jpg',
                         'video' =>
-                            'https://www.youtube.com/watch?v=k-CoAquRSwY',
+                            'https://www.youtube.com/embed/k-CoAquRSwY',
                     ],
                     [
                         'title' => 'Wildcat',
                         'description' =>
                             'Aussi appelé backflip, le wildcat est un salto arrière que le rider effectue dans les airs après avoir pris de la vitesse. C\'est un trick qui peut être difficile à réaliser puisque le snowboardeur doit veiller à rester dans le bon axe.',
                         'picture' =>
-                            'https://images.app.goo.gl/5Bm4JqqjkCy7VD8PA',
+                            'Wildcat.jpg',
                         'video' =>
-                            'https://www.youtube.com/watch?v=7KUpodSrZqI',
+                            'https://www.youtube.com/embed/7KUpodSrZqI',
                     ],
                 ],
             ],
@@ -115,27 +115,27 @@ class AppFixtures extends Fixture implements DependentFixtureInterface
                         'description' =>
                             "Comme on peut le deviner, les \"one foot\" sont des figures réalisées avec un pied décroché de la fixation. Pendant le saut, le snowboarder doit tendre la jambe du côté dudit pied. L'esthète Scotty Vine – une sorte de Danny MacAskill du snowboard – en a réalisé un bel exemple avec son Double Backflip One Foot.",
                         'picture' =>
-                            'https://images.app.goo.gl/ewihJiF6d9rrKCEe7',
+                            'DoubleBackflipOneFoot.jpg',
                         'video' =>
-                            'https://www.youtube.com/watch?v=ty5o0JMEBAQ',
+                            'https://www.youtube.com/embed/ty5o0JMEBAQ',
                     ],
                     [
                         'title' => 'Method air',
                         'description' =>
                             "Cette figure – qui consiste à attraper sa planche d'une main et le tourner perpendiculairement au sol – est un classique \"old school\". Il n'empêche qu'il est indémodable, avec de vrais ambassadeurs comme Jamie Lynn ou la star Terje Haakonsen. En 2007, ce dernier a même battu le record du monde du \"air\" le plus haut en s'élevant à 9,8 mètres au-dessus du kick (sommet d'un mur d'une rampe ou autre structure de saut).",
                         'picture' =>
-                            'https://images.app.goo.gl/4u2nmarkKpFjyhwG8',
+                            'MethodAir.jpg',
                         'video' =>
-                            'https://www.youtube.com/watch?v=ZWZGE9yp5hA',
+                            'https://www.youtube.com/embed/ZWZGE9yp5hA',
                     ],
                     [
                         'title' => 'Backside Triple Cork 1440',
                         'description' =>
                             " Comme on peut le deviner, les \"one foot\" sont des figures réalisées avec un pied décroché de la fixation. Pendant le saut, le snowboarder doit tendre la jambe du côté dudit pied. L'esthète Scotty Vine – une sorte de Danny MacAskill du snowboard – en a réalisé un bel exemple avec son Double Backflip One Foot.",
                         'picture' =>
-                            'https://images.app.goo.gl/kC5oo5qVSE8K5GWu9',
+                            'BacksideTripleCork1440.jpg',
                         'video' =>
-                            'https://www.youtube.com/watch?v=URFnYGzu9lU',
+                            'https://www.youtube.com/embed/URFnYGzu9lU',
                     ],
                 ],
             ],
@@ -148,18 +148,18 @@ class AppFixtures extends Fixture implements DependentFixtureInterface
                         'description' =>
                             "Le diminutif de corkscrew qui signifie littéralement tire-bouchon et désignait les premières simples rotations têtes en bas en frontside. Désormais, on utilise le mot cork à toute les sauces pour qualifier les figures où le rider passe la tête en bas, peu importe le sens de rotation. Et dorénavant en compétition, on parle souvent de double cork, triple cork et certains riders vont jusqu'au quadruple cork !",
                         'picture' =>
-                            'https://images.app.goo.gl/bQEJMrB12WfWS6Z28',
+                            'Cork.jpg',
                         'video' =>
-                            'https://www.youtube.com/watch?v=FMHiSF0rHF8',
+                            'https://www.youtube.com/embed/FMHiSF0rHF8',
                     ],
                     [
                         'title' => 'Double Backside Rodeo 1080',
                         'description' =>
                             "À l'instar du cork, le rodeo est une rotation désaxée, qui se reconnaît par son aspect vrillé. Un des plus beaux de l'histoire est sans aucun doute le Double Backside Rodeo 1080 effectué pour la première fois en compétition par le jeune prodige Travis Rice, lors du Icer Air 2007. La pirouette est tellement culte qu'elle a fini dans un jeu vidéo où Travis Rice est l'un des personnages.",
                         'picture' =>
-                            'https://images.app.goo.gl/HU8gcnuRCwFBwEpP6',
+                            'DoubleBacksideRodeo1080.jpg',
                         'video' =>
-                            'https://www.youtube.com/watch?v=hDSc7hQ0bzg',
+                            'https://www.youtube.com/embed/DSc7hQ0bzg',
                     ],
                 ],
             ],
@@ -172,9 +172,9 @@ class AppFixtures extends Fixture implements DependentFixtureInterface
                         'description' => "Mais qu’est-ce que le Jib ? C’est le fait de glisser sur des obstacles tels que des rails ou des box avec un snowboard. Il y a de nombreuses façons de faire du jib en snow. C’est aussi ce qui rend le concept si sympa – les possibilités infinies d’apprendre de nouveaux tricks. Et le mieux dans tout ça, c’est qu’une fois que les bases sont maîtrisées, on peut progresser assez rapidement.
                     La discipline royale du jib te permet de tester toutes tes compétences en street snow. Pour faire du jib dans les rues, pense à porter les meilleures vestes de snowboard et des pantalons de snowboard adaptés, car tu vas attirer les regards ! Les rois du jib de rue viennent principalement des villes du nord, bien enneigées en hiver, qui comptent peu de stations. Le snowboard de rue demande beaucoup d’habileté en snow, mais c’est aussi une nouvelle toile où exprimer sa créativité. Sebastian Toutant (plus connu sous le nom de Seb Toots) est l’un de ces snowboarders talentueux qui parvient à faire des rues enneigées du Québec son propre snowpark. Ne nous voilons pas la face, personne ne sera jamais aussi dur à cuire. Mais on peut déjà essayer d’apprendre les rudiments du jib en s’inspirant des idées de ce roi du snow urbain.",
                         'picture' =>
-                            'https://images.app.goo.gl/uH3wrHm7mjJdGV767',
+                            'Jib.jpg',
                         'video' =>
-                            'https://www.youtube.com/watch?v=5mxLMI4w_wE',
+                            'https://www.youtube.com/embed/5mxLMI4w_wE',
                     ],
                 ],
             ],
@@ -205,6 +205,7 @@ class AppFixtures extends Fixture implements DependentFixtureInterface
 
                 $picture = new Picture();
                 $picture->setName($figureData['picture']);
+                $picture->setSortOrder(1);
                 $manager->persist($picture);
 
                 $video = new Video();
@@ -215,7 +216,7 @@ class AppFixtures extends Fixture implements DependentFixtureInterface
                 $figure->addPicture($picture);
                 $figure->addVideo($video);
 
-                for ($k = 0; $k < mt_rand(20, 40); $k++) {
+                for ($k = 0; $k < mt_rand(12, 20); $k++) {
                     $comment = new Comment();
                     $days = $now->diff($figure->getCreatedAt())->days;
 
